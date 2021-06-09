60dee8de87a776589f505ad5d506b0a66b113e65
Logan_Sel <logansel59162@gmail.com>
2021-06-09 09:38:49 +0200

    nepastenircompte

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -44,48 +44,51 @@ const Navbar: React.FC = () => {
           </li> */}
         </ul>
         {/* dropdown */}
-        <div className="dropdown ">
-          <button
-            className="btn btn-secondary dropdown-toggle"
-            type="button"
-            id="dropdownMenuButton1"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="7em"
-              height="2em"
-              fill="currentColor"
-              className="bi bi-person-circle"
-              viewBox="0 0 16 16"
+        <div className="container">
+          {/* <div className="dropdown d-flex flex-row-reverse"> */}
+          <div className="dropdown d-flex justify-content-end">
+            <button
+              className="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
             >
-              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-              <path
-                fillRule="evenodd"
-                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-              />
-            </svg>
-          </button>
-          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li>
-              <Link href="/profile">
-                <a className="dropdown-item">Account</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/api/login">
-                <a className="dropdown-item">Login</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/api/logout">
-                <a className="dropdown-item" href="/api/logout">
-                  Logout
-                </a>
-              </Link>
-            </li>
-          </ul>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="7em"
+                height="2em"
+                fill="currentColor"
+                className="bi bi-person-circle"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                <path
+                  fillRule="evenodd"
+                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                />
+              </svg>
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li>
+                <Link href="/profile">
+                  <a className="dropdown-item">Account</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/api/login">
+                  <a className="dropdown-item">Login</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/api/logout">
+                  <a className="dropdown-item" href="/api/logout">
+                    Logout
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
